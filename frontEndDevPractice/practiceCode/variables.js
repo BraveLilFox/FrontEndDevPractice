fd73bc7e1bb49908d27ff34bd8f64d8641391445
Kaide Ryder <157544440+BraveLilFox@users.noreply.github.com>
2024-07-06 15:54:01 -0400

    Update and rename javaScript.js to variables.js

---
+++ frontEndDevPractice/practiceCode/variables.js
@@ -0,0 +1,35 @@
+// variable = A container that stores a value.
+//            Behaves as if it were a value it contains.
+
+// 1. declaration    let x;
+// 2. assignment     x = 100;
+
+// These are number variables
+let age = 8;
+let price = 10.99;
+let gpa = 3.1;
+
+console.log(`You are ${age} years old.`);
+console.log(`The price will be ${price} today`);
+console.log(`Your GPA is ${gpa}`);
+console.log(typeof gpa);
+
+// These are string variables
+let firstName = `Kaide`
+let favFood = `Macaroni`
+let email = `kkpotchka415@gmail.com`
+
+console.log(firstName);
+console.log(`You like ${favFood}, right?`);
+console.log(`Your email is ${email}`);
+console.log(typeof email)
+
+// These are Boolean Variables
+let forSale = true;
+let student = false;
+let online = false;
+
+console.log(`Online: ${online}`);
+console.log(`Is this car for sale: ${forSale}`);
+console.log(`Enrolled: ${student}`);
+console.log(typeof online);
