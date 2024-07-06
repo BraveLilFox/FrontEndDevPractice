909e3914d8be4a2c82a188a6df081ce659c6e727
Kaide Ryder <157544440+BraveLilFox@users.noreply.github.com>
2024-07-06 18:13:09 -0400

    Create mathObject.js

---
+++ frontEndDevPractice/practiceCode/mathObject.js
@@ -0,0 +1,31 @@
+// Math = built-in object that provides a
+//        collection of properties and methods
+
+//console.log(Math.PI);
+//console.log(Math.E);
+
+let x = 3;
+let y = 2;
+let z = 1;
+
+//z = Math.round(x); follows the general rounding rules
+//z = Math.floor(x); Will always round down
+//z = Math.ceil(x); Will always round up
+//z = Math.trunc(x); Will elimate all decimal portions
+
+//z = Math.pow(x, y); raised the the power of
+//z = Math.sqrt(x); squareroot
+//z = Math.log(x); natural log function
+
+//z = Math.sin(x); sin function
+//z = Math.cos(x); cosin function
+//z = Math.tan(x); tangent function
+
+//z = Math.abs(x); absolute value function
+//z = Math.sign(x); determines neg or pos of value
+
+let max = Math.max(x, y, z);
+let min = Math.min(x, y, z);
+
+console.log(max);
+console.log(min);
