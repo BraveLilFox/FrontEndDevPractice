6a82c1abb274d8a75a24550446d955df95dd82f2
Kaide Ryder <157544440+BraveLilFox@users.noreply.github.com>
2024-07-06 15:43:39 -0400

    Update javaScript.js

--- frontEndDevPractice/practiceCode/javaScript.js
+++ frontEndDevPractice/practiceCode/javaScript.js
@@ -3,3 +3,10 @@
 
 // 1. declaration    let x;
 // 2. assignment     x = 100;
+
+let age = 8;
+let price = 10.99;
+let gpa = 3.1;
+// these are all number variables
+
+console.log(`You are ${age} years old`);
