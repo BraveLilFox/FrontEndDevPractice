51e61047b1e10cdf4cf5725a7d417329dadc674d
Kaide Ryder <157544440+BraveLilFox@users.noreply.github.com>
2024-07-09 23:23:08 -0400

    Update ifStatements.js

--- frontEndDevPractice/practiceCode/ifStatements.js
+++ frontEndDevPractice/practiceCode/ifStatements.js
@@ -1,3 +1,52 @@
 // IF STATEMENTS = if a condition is true, execute some code
 //                 if not, do something different
 
+// Ex. 1
+let age = 17;
+
+if(age >= 18){
+    console.log("You are old enough to enter this site");
+}
+else{
+    console.log("You must be 18yrs to see me");
+}
+
+// Ex. 2
+let time = 14;
+
+if(time < 12){
+    console.log("Good morning!");
+}
+else{
+    console.log("Good afternoon!");
+}
+
+// Ex. 3
+let isStudent = true;
+
+if(isStudent){
+    console.log("You are a student.");
+}
+else{
+    console.log("You are not a student.");
+}
+
+// Ex. 4
+let age = 22;
+let doesDrink = false;
+
+if(age >= 21){
+    console.log("You can legally drink!")
+
+    if(doesDrink){
+        console.log("You drink!");
+    }
+    else{
+        console.log("You do not drink.")
+    }
+}
+else{
+    console.log("You cannot legally drink!")
+}
+
+//Ex. 5
