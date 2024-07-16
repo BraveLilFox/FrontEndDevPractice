742b999909321f19edf9088b8800cbcaf73ea1e7
Kaide Ryder <157544440+BraveLilFox@users.noreply.github.com>
2024-07-16 13:17:18 -0400

    Update switches.js

--- frontEndDevPractice/practiceCode/switches.js
+++ frontEndDevPractice/practiceCode/switches.js
@@ -1,3 +1,30 @@
 // SWITCH = can be an efficient replacement to many else is statements
 
+// Ex 1
+let day = 1;
 
+switch(day){
+    case 1:
+        console.log(`It is MONDAY`);
+        break;
+    case 2:
+        console.log(`It is TUESDAY`);
+        break;
+    case 3:
+        console.log(`It is WEDNESDAY`);
+        break;
+    case 4:
+        console.log(`It is THURSDAY`);
+        break;
+    case 5:
+        console.log(`It is FRIDAY`);
+        break;
+    case 6:
+        console.log(`It is SATURDAY`);
+        break;
+    case 7:
+        console.log(`It is SUNDAY`);
+        break;
+    default:
+        console.log(`${day} is not a day`)
+}
